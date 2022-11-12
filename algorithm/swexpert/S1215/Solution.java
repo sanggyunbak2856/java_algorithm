5bb2a3c870963f7b55d6a2545db99b6ff8a49ac8
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-11-12 14:54:14 +0900

    1215

--- algorithm/swexpert/S1215/Solution.java
+++ algorithm/swexpert/S1215/Solution.java
@@ -5,42 +5,46 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class Solution {
-    static char[][] map;
     static int n;
-    static boolean checkPalindromeMap(int start, int end, int k, boolean mode) { // mode true -> horizontal, false -> verticle
-        int len = end - start;
-        if (mode) {
-            for(int i = 0; i < len / 2; i++) {
-                if(map[k][i] == map[k][end - i]) continue;
-                else return false;
-            }
-            return true;
+    static char[][] map;
+    static boolean checkPalindromeHorizontal(int start, int end, int row) { // start, end는  x인덱스
+        int len = (end - start + 1) / 2;
+        for(int i = 0; i < len; i++){
+            if(map[row][start + i] == map[row][end - i]) continue;
+            else return false;
         }
-        else {
-            for(int i = 0; i < len / 2; i++) {
-                if(map[i][k] == map[end - i][k]) continue;
-                else return false;
-            }
-            return true;
+        return true;
+    }
+    static boolean checkPalindromeVerticle(int start, int end, int column) { // start, end는  y인덱스
+        int len = (end - start + 1) / 2;
+        for(int i = 0; i < len; i++){
+            if(map[start + i][column] == map[end - i][column]) continue;
+            else return false;
         }
+        return true;
     }
     static int countPalindrome() {
         int count = 0;
 
-        // 가로, 세로
         for(int i = 0; i < 8; i++) {
             for(int j = 0; j < 8 - n + 1; j++) {
-                if(checkPalindromeMap(j, j + n - 1, i, true)) count+=1;
-                if(checkPalindromeMap(j, j + n - 1, i, false)) count+=1;
+                if(checkPalindromeHorizontal(j, j + n - 1, i)) count++;
+            }
+        }
+
+        for(int i = 0; i < 8 - n + 1; i++) {
+            for(int j = 0; j < 8; j++) {
+                if(checkPalindromeVerticle(i, i + n - 1, j)) count++;
             }
         }
 
         return count;
     }
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
+
         for(int test_case = 1; test_case <= T; test_case++) {
             n = Integer.parseInt(br.readLine());
             map = new char[8][];
@@ -49,8 +53,7 @@ public class Solution {
             }
 
             int count = countPalindrome();
-            System.out.printf("#%d %d", test_case, count);
-
+            System.out.printf("#%d %d\n", test_case, count);
         }
 
         br.close();
