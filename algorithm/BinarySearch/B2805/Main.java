97d39812cc8e2a0733310481e1ffd8e5d2748dfc
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-01-12 09:38:43 +0900

    B2805

--- algorithm/BinarySearch/B2805/Main.java
+++ algorithm/BinarySearch/B2805/Main.java
@@ -1,50 +1,45 @@
 package algorithm.BinarySearch.B2805;
 
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
-
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-    static long answer = 0;
-    static void findHeight(int[] arr, int m, int max, int min) {
-        while(true) {
-            int mid = (min + max) / 2;
-            long sum = 0;
-
-            for(int i = 0; i < arr.length; i++) {
-                int diff = arr[i] - mid;
-                if(diff > 0) sum += diff;
-            }
-
-            if(sum == m) {
-                answer = mid;
-                return;
-            }
-            else if(sum < m) max = mid;
-            else min = mid + 1;
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int[] arr = new int[n];
-
         st = new StringTokenizer(br.readLine());
         int max = 0;
-        int min = Integer.MAX_VALUE;
         for(int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             max = arr[i] > max ? arr[i] : max;
-            min = min > arr[i] ? min : arr[i];
         }
 
-        findHeight(arr, m, max, min);
-        System.out.println(answer);
+        int min = 0;
+        int mid = 0;
+        while(min < max) {
+            mid = (max + min) / 2;
+            long sum = 0;
+            for(int i = 0; i < n; i++) {
+                if(arr[i] > mid) sum += (arr[i] - mid);
+            }
+
+            if(sum < m) max = mid;
+            else min = mid + 1;
+        }
+
+        bw.write(min - 1 + "\n");
+        bw.flush();
+
         br.close();
+        bw.close();
     }
 }
