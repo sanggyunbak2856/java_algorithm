1e9323a338ad23403128f6d99bb7e6cc3fcb0ba2
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-01-21 23:48:02 +0900

    b2493

---
+++ algorithm/Baekjun/B1000/Main.java
@@ -0,0 +1,25 @@
+package algorithm.Baekjun.B1000;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        bw.write(n + m + "\n");
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
