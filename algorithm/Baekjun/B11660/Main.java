31af66de55666792f43d0740a349255a732e77c6
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-01-26 10:49:35 +0900

    B11660

--- algorithm/Baekjun/B11660/Main.java
+++ algorithm/Baekjun/B11660/Main.java
@@ -15,23 +15,32 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[][] map = new int[n + 1][n + 1];
-        for(int i = 1; i < n + 1; i++){
+        int[][] arr = new int[n + 1][n + 1];
+
+        for(int i = 1; i < n + 1; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 1; j < n + 1; j++) {
-                map[i][j] = map[i][j - 1] + Integer.parseInt(st.nextToken());
+                arr[i][j] = arr[i][j - 1] + Integer.parseInt(st.nextToken());
             }
         }
 
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int y1 = Integer.parseInt(st.nextToken());
             int x1 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
-            
-            
+
+            for(int j = y1; j <= y2; j++) {
+                answer += (arr[j][x2] - arr[j][x1 - 1]);
+            }
+            sb.append(answer).append("\n");
+            answer = 0;
         }
+        bw.write(sb.toString());
+        bw.flush();
 
         br.close();
         bw.close();
