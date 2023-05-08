9ecf6f63b1b28f7f9df2d07b3e2e163ced2f5c88
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-05-08 17:15:51 +0900

    B1644

---
+++ algorithm/Baekjun/B1644/Main.java
@@ -0,0 +1,71 @@
+package algorithm.Baekjun.B1644;
+
+import java.io.*;
+
+public class Main {
+    static int calc(int n, boolean[] isPrime) {
+        int sum = 2;
+        int left = 2;
+        int right = 2;
+        int count = 0;
+
+        while(left <= right) {
+            if(right > n) break;
+            if(sum == n) {
+                count += 1;
+                right += 1;
+                if(right > 4000000) break;
+                while(isPrime[right] | right == 2) {
+                    if(right == 4000000) break;
+                    right += 1;
+                }
+                sum += right;
+            }
+            else if(sum < n) {
+                right += 1;
+                if(right > 4000000) break;
+                while(isPrime[right] || right == 2) {
+                    if(right == 4000000) break;
+                    right += 1;
+                }
+                sum += right;
+            }
+            else {
+                sum -= left;
+                left += 1;
+                if(left > 4000000) break;
+                while(isPrime[left] || left == 2) {
+                    if(left == 4000000) break;
+                    if(left > right) break;
+                    left += 1;
+                }
+            }
+        }
+
+        return count;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        boolean[] isPrime = new boolean[4000001]; // false -> 소수, true -> 소수 아님
+        isPrime[0] = true; isPrime[1] = true;
+
+        for(int i = 2; i < Math.sqrt(isPrime.length); i++) {
+            if(!isPrime[i]) {
+                for(int j = 2 * i; j < 4000001; j += i) {
+                    isPrime[j] = true;
+                }
+            }
+        }
+
+        for(int i = 0; i < 1; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int answer = calc(n, isPrime);
+            bw.write(answer + "\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
